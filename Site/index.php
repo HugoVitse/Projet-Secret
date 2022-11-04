09bb0e2acea4fd9c8095b9e11b5675cbed2517c2
HugoVitse <90216651+HugoVitse@users.noreply.github.com>
2022-11-04 21:23:05 +0100

    push

---
+++ Site/index.php
@@ -0,0 +1,42 @@
+<html>
+
+
+    <head>
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+
+
+    <body>
+        <img id="background" src="images/bg.png">
+        <a href="index.php" id="logo"><img  src="images/logo.png"></a>
+
+        <a class="gamemode" id="classic" href="classic.php">
+            <img src="images/classic.png">
+            <p class="namegame"> Devinez le personnage </p>
+        </a>
+        <a class="gamemode" id="quote" href="bounty.php">
+            <img  src="images/quote.png">
+            <p class="namegame"> Devinez l'auteur de la citation </p>
+        </a>
+        <a class="gamemode" id="bounty" href="quote.php">
+            <img  src="images/bounty.png">
+            <p class="namegame"> Devinez la prime </p>
+        </a>
+        <a class="gamemode" id="splash" href="splash.php">
+            <img  src="images/splash.png">
+            <p class="namegame"> Devinez le personnage sur le splash art </p>
+        </a>
+        <div id="resaux">
+            <a id="twitter">
+                <img src="images/twitter.png">
+            </a>
+            <div id="infos">
+                <img src="images/infos.png">
+            </div>
+            <p id="copyright"> Onedle - 2022 </p>
+        </div>
+    </body>
+
+
+
+</html>
